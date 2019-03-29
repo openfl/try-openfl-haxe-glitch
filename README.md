0e804f2f79b68545aef03d0356d8a6de1940da22
Joshua Granick <jgranick@users.noreply.github.com>
2019-03-29 10:54:42 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# try-openfl-haxe-glitch
+# Try OpenFL
+
+This is a Haxe sandbox for trying [OpenFL](https://www.openfl.org). Hit the "Remix" button in order to begin making changes!
+
+Hit the "Show" button to see a live preview. Changes to your application (such as "src/App.hx") will compile automatically and the preview will refresh, but if there are compile errors, the debug console in your live preview tab will tell you what is wrong.
---
+++ src/App.hx
@@ -0,0 +1,22 @@
+import js.Browser.document;
+import js.Lib.require;
+import openfl.display.Sprite;
+import openfl.display.Stage;
+
+class App extends Sprite
+{
+	public function new()
+	{
+		super();
+		
+		var icon = new Loader();
+		icon.load(new URLRequest(require("./assets/icon.png")));
+		addChild(icon);
+	}
+	
+	static function main()
+	{
+		var stage = new Stage (550, 400, 0xFFFFFF, App);
+		document.body.appendChild (stage.element);
+	}
+}
